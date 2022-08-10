b7bba6650086880597ca00b8b9d5002fc0f2e6b9
SamwiseLA <github@samwiser.com>
2022-08-10 13:09:18 -0700

    Change Girl From Ipanema to mp3 / ios not handling ogg

--- src/my_scene00_objects.ts
+++ src/my_scene00_objects.ts
@@ -248,8 +248,8 @@ export default class MySceneObjects {
     //  "https://cdn-content-ingress.altvr.com/uploads/audio_clip/audio/1793021533619224851/ogg_220173__gameaudio__spacey-1up-power-up.ogg";
 
     const uri =
-      "https://cdn-content-ingress.altvr.com/uploads/audio_clip/audio/1734282589813867336/ogg_Girl_From_Ipanema_-_Frank_Sinatra.ogg";
-
+      //"https://cdn-content-ingress.altvr.com/uploads/audio_clip/audio/1734282589813867336/ogg_Girl_From_Ipanema_-_Frank_Sinatra.ogg";
+      "https://dl.dropbox.com/s/rgm7xqiguux0t0d/Girl%20From%20Ipanema%20-%20Frank%20Sinatra.mp3"
     //this.appMain.music = new BABYLON.Sound("cello", "sounds/cellolong.wav", this.appMain._scene, null, { loop: true, autoplay: true });
 
     //this.appMain.music = new BABYLON.Sound("sound2", uri, this.appMain._scene);
